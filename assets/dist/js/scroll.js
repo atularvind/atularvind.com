c09573641744354626656b1f21bb1bb9fb984619
AtulDeploy <atul.surficle@gmail.com>
2016-10-16 11:29:00 -0700

    Auto Deploy pages by atul

--- assets/dist/js/scroll.js
+++ assets/dist/js/scroll.js
@@ -1,15 +1,3 @@
-//$('#nav').affix({
-//    offset: {
-//      top: $('#nav').offset().top-20,
-//      bottom: ($('.footer').outerHeight(true)+ 
-//    		  $('.social').outerHeight(true)+
-//    		  $('.disqus').outerHeight(true)+
-//    		  500)
-//    }
-//});
-//
-//console.log( $('#disqus_thread').height());
-
 var timesRun = 0;
 var interval = setInterval(function() {
 	timesRun += 1;
@@ -21,7 +9,6 @@ var interval = setInterval(function() {
         		  $('#disqus_thread').outerHeight(true) + 500)
         }
     });
-    console.log(timesRun);
     if(timesRun === 10){
         clearInterval(interval);
     }
